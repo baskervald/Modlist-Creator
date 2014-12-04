1949452023137ade3661299dcb0588ded4ef253f
Ashlynn <ashlynn.wonderland@gmail.com>
2014-12-04 10:14:19 -0500

    fin

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var input = document.querySelector('#main');
+input.onkeydown = function ( e ){
+  if( e.keyCode === 13 ){
+    submit();
+  }
+};
+
+function submit(){
+  input.value = 'https://ml.reddit.com/r/themoddingofisaac/search?q=flair%3Arelease+author%3A' + encodeURIComponent(input.value);
+}

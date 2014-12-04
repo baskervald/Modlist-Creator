90882168369b168c4c9565b5fec4b125b093341b
Ashlynn <ashlynn.wonderland@gmail.com>
2014-12-04 10:23:21 -0500

    fancy

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 var input = document.querySelector('#main');
+var button = document.querySelector('#button');
+var url = document.querySelector('#url');
+
 input.onkeydown = function ( e ){
   if( e.keyCode === 13 ){
     submit();
   }
 };
+button.onclick = submit;
 
 function submit(){
-  input.value = 'https://ml.reddit.com/r/themoddingofisaac/search?q=flair%3Arelease+author%3A' + encodeURIComponent(input.value) + '&restrict_sr=on';
+  url.textContent = 'https://ml.reddit.com/r/themoddingofisaac/search?q=flair%3Arelease+author%3A' + encodeURIComponent(input.value) + '&restrict_sr=on';
 }

5b8f7c4bd4b7b80ab376e4ac160faebdcd4d110c
Ashlynn <ashlynn.wonderland@gmail.com>
2014-12-08 14:31:05 -0500

    generating short urls now

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var input = document.querySelector('#main');
 var button = document.querySelector('#button');
 var url = document.querySelector('#url');
+var shorturl = document.querySelector('#shorturl')
 
 input.onkeydown = function ( e ){
   if( e.keyCode === 13 ){
@@ -10,6 +11,9 @@ input.onkeydown = function ( e ){
 button.onclick = submit;
 
 function submit(){
+  var encoded = encodeURIComponent(input.value);
   url.className = 'fancyurl';
-  url.textContent = 'https://ml.reddit.com/r/themoddingofisaac/search?q=flair%3Arelease+author%3A' + encodeURIComponent(input.value) + '&restrict_sr=on';
+  shorturl.className = 'fancyurl';
+  url.textContent = 'https://ml.reddit.com/r/themoddingofisaac/search?restrict_sr=on&q=flair%3Arelease+author%3A' + encoded;
+  shorturl.textContent = 'https://ashlynninwonderland.github.io/Modlist-Creator/redirect.html#' + encoded;
 }
